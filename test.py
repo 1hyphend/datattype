1caef33e5496eb9d1944daf1675a9af5222f0254
1hyphend <Ivanc221@nycstudents.net>
2025-09-19 10:03:10 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,39 @@
+# Bill= input("How much is bill")  
 
-x = 100
-y = float(32)
-print(x,y)
-tip = x*0.18
-bill=9
-total = tip + bill
-print = input("How much is bill")  
+# bill=float(Bill)
+# tip=bill*0.18
+
+# total = tip + bill
+# print(str("your total is")+str(total))
+
+
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+#     print(values[0])
+# print(values[6])
+
+# x = "this is a thing"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+
+
+# x = input("do you wanna buy a thing?(y/n)")
+
+# while x is not "n":
+#     item = input("what ya wanna buy")
+# print("thanks for your purchase")
+# y = random.randint(1,100)
+# print(y)
+
+# x=56
+# def test():
+#     x = 50
+# test(x)
+# print(x)
+# x =0
+# while True:
+#     print("running")
