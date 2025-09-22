e87a43a2a45945e28b5d8186ee1d06af8d586919
1hyphend <Ivanc221@nycstudents.net>
2025-09-22 10:14:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,42 @@
-Bill= input("How much is bill")  
+# Bill= input("How much is bill")  
 
-bill=float(Bill)
-tip=bill*0.18
+# bill=float(Bill)
+# tip=bill*0.18
 
-total = tip + bill
-print(str("your total is")+str(total))
-
-
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
-    print(values[0])
-print(values[6])
-
-x = "this is a thing"
-y= x.split( )
-print(y)
-print(z)
+# total = tip + bill
+# print(str("your total is")+str(total))
 
 
-x = input("do you wanna buy a thing?(y/n)")
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+#     print(values[0])
+# print(values[6])
 
-while x is not "n":
-    item = input("what ya wanna buy")
-print("thanks for your purchase")
-y = random.randint(1,100)
-print(y)
+# x = "this is a thing"
+# y= x.split( )
+# print(y)
+# print(z)
 
-x=56
-def test():
-    x = 50
-test(x)
-print(x)
-x =0
-while True:
-    print("running")
+
+# x = input("do you wanna buy a thing?(y/n)")
+
+# while x is not "n":
+#     item = input("what ya wanna buy")
+# print("thanks for your purchase")
+# y = random.randint(1,100)
+# print(y)
+
+# x=56
+# def test():
+#     x = 50
+# test(x)
+# print(x)
+# x =0
+# while True:
+#     print("running")
+
+
+x=input("input a sentence")
+y=x.split
